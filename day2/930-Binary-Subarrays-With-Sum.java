26c3b09fa9adb67a247b8646ce8f8b3a9812e4db
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-02-19 15:40:23 +0530

    [930] [Time Beats: 100.00%] [Memory Beats: 75.16%] - LeetPush

---
+++ day2/930-Binary-Subarrays-With-Sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numSubarraysWithSum(int[] a, int goal) {
+        //to count number of subarryas we can use
+        //count subarrays atmost k -count csubarrays atmoct k-1
+        return count(a,goal)-count(a,goal-1);
+        
+        
+    }
+    public int count(int []a,int goal){
+         if (goal < 0) return 0; 
+        int r=0;
+        int l=0;
+        int sum=0;
+        int n=a.length;
+        int cnt=0;
+        while(r<n){
+            sum=sum+a[r];
+            while(sum>goal){
+                if(a[l]==1){
+                    sum=sum-a[l];
+                }
+                l++;
+            }
+
+
+            if(sum<=goal){
+                cnt=cnt+(r-l+1);
+            }
+            r++;
+        }
+        return cnt;
+    }
+}
