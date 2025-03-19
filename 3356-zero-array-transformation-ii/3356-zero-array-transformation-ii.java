8e7a8114663b500f4de46c017b26a1fb2aafb9ea
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-03-19 05:54:17 +0530

    Time: 27 ms (20.64%), Space: 122.7 MB (41.39%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
@@ -1,24 +1,35 @@
 class Solution {
-    public int minZeroArray(int[] a, int[][] query) {
-        int n=a.length;
-        int diff[]=new int[n+1];
-        int pos=0;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            while(sum+diff[i]<a[i]){
-                if(pos==query.length){
-                    return -1;
-                }
-                int start=query[pos][0];
-                int end=query[pos][1];
-                int val=query[pos][2];
-                pos++;
-                if(end<i) continue;
-                diff[Math.max(start,i)]+=val;
-                diff[end+1]-=val;
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int left = 0, right = queries.length, ans = -1;
+
+        // Binary search to find the smallest k
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (canZeroArray(nums, queries, mid, n)) {
+                ans = mid;  // Try for smaller k
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-            sum+=diff[i];
         }
-        return pos;
+        return ans;
+    }
+
+    private boolean canZeroArray(int[] nums, int[][] queries, int k, int n) {
+        int[] diff = new int[n + 1]; // Difference array
+        for (int i = 0; i < k; i++) {
+            int li = queries[i][0], ri = queries[i][1], val = queries[i][2];
+            diff[li] -= val;
+            diff[ri + 1] += val;
+        }
+
+        // Apply the difference array
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += diff[i];
+            if (nums[i] + sum > 0) return false; // If any element remains nonzero
+        }
+        return true;
     }
 }
