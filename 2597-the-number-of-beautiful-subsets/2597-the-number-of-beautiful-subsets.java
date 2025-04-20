be613568c27e35e9a07189e2c90e3e61767b285b
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-04-20 20:42:34 +0530

    Time: 353 ms (36.65%), Space: 45.2 MB (47.33%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -0,0 +1,25 @@
+class Solution {
+    private Map<Integer, Integer> freq = new HashMap<>();
+
+    private int dfs(int[] nums, int k, int i) {
+        if (i == nums.length) return 1;
+
+        int notTake = dfs(nums, k, i + 1); // Skip nums[i]
+        int take = 0;
+
+        // Take nums[i] only if nums[i] - k not already taken
+        if (!freq.containsKey(nums[i] - k)) {
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+            take = dfs(nums, k, i + 1);
+            freq.put(nums[i], freq.get(nums[i]) - 1);
+            if (freq.get(nums[i]) == 0) freq.remove(nums[i]);
+        }
+
+        return take + notTake;
+    }
+
+    public int beautifulSubsets(int[] nums, int k) {
+        Arrays.sort(nums); // Important to ensure same numbers are grouped
+        return dfs(nums, k, 0) - 1; // Exclude empty subset
+    }
+}
