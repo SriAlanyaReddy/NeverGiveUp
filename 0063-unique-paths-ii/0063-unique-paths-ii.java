5c686bd4fee5dde2b2425a9cd7f2ffd5209cab2b
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-04-24 20:58:34 +0530

    Time: 0 ms (100%), Space: 42 MB (39.77%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,24 +1,35 @@
+import java.util.Arrays;
+
 class Solution {
     public int uniquePathsWithObstacles(int[][] a) {
         int n = a.length;
         int m = a[0].length;
 
-        // If the starting or ending point is an obstacle, return 0
-        if (a[0][0] == 1 || a[n - 1][m - 1] == 1) return 0;
+        int[][] dp = new int[n][m];
 
-        int dp[][] = new int[n][m];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);  // Initialize dp with -1
+        // Start point
+        if (a[0][0] == 1) return 0;
+        dp[0][0] = 1;
+
+        // Fill first column
+        for (int i = 1; i < n; i++) {
+            if (a[i][0] == 0) dp[i][0] = dp[i-1][0];
         }
 
-        return findways(dp, n - 1, m - 1, a);
-    }
-    public int findways(int[][] dp,int i,int j,int[][] a){
-        if(i==0 && j==0) return 1;
-        if((i<0 || j<0) || a[i][j]==1 ) return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int top=findways(dp,i,j-1,a);
-        int left=findways(dp,i-1,j,a);
-        return dp[i][j]=top+left;
+        // Fill first row
+        for (int j = 1; j < m; j++) {
+            if (a[0][j] == 0) dp[0][j] =dp[0][j-1];
+        }
+
+        // Fill the rest of the dp table
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (a[i][j] == 0) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
     }
 }
