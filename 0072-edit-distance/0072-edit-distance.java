f45456c594cae48fe80a79957e9bd56703b29be2
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-05-05 12:45:17 +0530

    Time: 5 ms (64.21%), Space: 45.1 MB (62.93%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -5,8 +5,26 @@ class Solution {
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+              if (word1.charAt(i-1) == word2.charAt(j-1)) {
+            dp[i][j] = dp[i - 1][j - 1];
+        } else {
+            int insert = dp[i][j - 1];
+            int delete = dp[i-1][j];
+            int replace =dp[i-1][j - 1];
+            dp[i][j] = 1 + Math.min(Math.min(insert, delete), replace);
+        }
+            }
+        }
 
-        return findMin(word1, word2, n , m, dp);
+        return dp[n][m];
     }
 
     public int findMin(String word1, String word2, int i, int j, int[][] dp) {
