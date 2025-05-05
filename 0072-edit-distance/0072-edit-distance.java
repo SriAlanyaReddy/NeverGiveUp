c01bac5b61b1e856e9142d48baa4c129d23baf6c
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-05-05 12:40:07 +0530

    Time: 3 ms (95.2%), Space: 44.9 MB (78.13%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,20 +1,20 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length(), m = word2.length();
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
 
-        return findMin(word1, word2, n - 1, m - 1, dp);
+        return findMin(word1, word2, n , m, dp);
     }
 
     public int findMin(String word1, String word2, int i, int j, int[][] dp) {
-        if (i < 0) return j + 1;  // If word1 is empty, insert all of word2[0...j]
-        if (j < 0) return i + 1;  // If word2 is empty, delete all of word1[0...i]
+        if (i == 0) return j ;  // If word1 is empty, insert all of word2[0...j]
+        if (j == 0) return i;  // If word2 is empty, delete all of word1[0...i]
         if (dp[i][j] != -1) return dp[i][j];
 
-        if (word1.charAt(i) == word2.charAt(j)) {
+        if (word1.charAt(i-1) == word2.charAt(j-1)) {
             return dp[i][j] = findMin(word1, word2, i - 1, j - 1, dp);
         } else {
             int insert = findMin(word1, word2, i, j - 1, dp);
