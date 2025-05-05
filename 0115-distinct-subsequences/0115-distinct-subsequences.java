50d03ea124d62fb1f28e2a78b6a3f2f969b2c932
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-05-05 12:06:13 +0530

    Time: 20 ms (44.83%), Space: 51.6 MB (18.63%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -2,21 +2,21 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n=s.length(),m=t.length();
 
-        int dp[][]=new int[n][m];
+        int dp[][]=new int[n+1][m+1];
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        return finddistinct(s,t,n-1,m-1,dp);
+        return finddistinct(s,t,n,m,dp);
     }
     public int finddistinct(String s,String t,int ind1,int ind2,int[][] dp){
-        if(ind2<0){
+        if(ind2==0){
             return 1;
         }
-        if(ind1<0){
+        if(ind1==0){
             return 0;
         }
         if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(s.charAt(ind1)==t.charAt(ind2)){
+        if(s.charAt(ind1-1)==t.charAt(ind2-1)){
             dp[ind1][ind2]=finddistinct(s,t,ind1-1,ind2-1,dp)+finddistinct(s,t,ind1-1,ind2,dp);
         }
         else{
