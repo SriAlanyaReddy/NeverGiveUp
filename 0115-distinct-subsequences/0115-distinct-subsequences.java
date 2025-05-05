dc7e097fda85f59a8866c96cf2b6e5043a5ec4e2
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-05-05 12:08:13 +0530

    Time: 19 ms (64.48%), Space: 51.3 MB (57.56%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,27 +1,27 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int n=s.length(),m=t.length();
+        int n = s.length(), m = t.length();
 
-        int dp[][]=new int[n+1][m+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Base Case Initialization
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;  // Empty string 't' is a subsequence of any prefix of 's'
         }
-        return finddistinct(s,t,n,m,dp);
-    }
-    public int finddistinct(String s,String t,int ind1,int ind2,int[][] dp){
-        if(ind2==0){
-            return 1;
+
+        // Fill the table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    // Include + Exclude
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    // Exclude only
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
-        if(ind1==0){
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(s.charAt(ind1-1)==t.charAt(ind2-1)){
-            dp[ind1][ind2]=finddistinct(s,t,ind1-1,ind2-1,dp)+finddistinct(s,t,ind1-1,ind2,dp);
-        }
-        else{
-            dp[ind1][ind2]=finddistinct(s,t,ind1-1,ind2,dp);
-        }
-        return dp[ind1][ind2];
+
+        return dp[n][m];
     }
 }
