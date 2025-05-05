39e09cdbe41233c330688c72c3d0185472de6756
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-05-05 18:48:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int dp[][]=new int[n][2];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        int buy=1;
+        return findmaxprofit(prices,0,buy,dp);
+    }
+    public int findmaxprofit(int[] prices,int ind,int buy,int[][] dp){
+        int n=prices.length;
+        if(ind==n){
+            return 0;
+        }
+        int profit;
+        if(buy==1){
+            int take=-prices[ind]+findmaxprofit(prices,ind+1,0,dp);
+             int nottake=findmaxprofit(prices,ind+1,1,dp);
+             profit=Math.max(take,nottake);
+        }
+        else{
+            int take=prices[ind]+findmaxprofit(prices,ind+1,1,dp);
+            int nottake=findmaxprofit(prices,ind+1,0,dp);
+             profit=Math.max(take,nottake);
+
+        }
+        return dp[ind][buy]=profit;
+    }
+}
