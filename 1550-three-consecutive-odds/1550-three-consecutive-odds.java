629a052e8088a8670ba9a8affda373ee2a96658d
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-05-17 19:59:06 +0530

    Time: 0 ms (100%), Space: 42.7 MB (15.76%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] a) {
+        int n=a.length,c=0;
+
+        for(int i=0;i<n;i++){
+            if(a[i]%2!=0){
+                c++;
+                if(c==3){
+                    return true;
+                }
+            }
+            else{
+                c=0;
+            }
+        }
+        return false;
+    }
+}
