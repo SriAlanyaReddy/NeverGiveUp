9dcafee7183a2952c5b23ff7b086bef4be1cbf05
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-05-19 21:25:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFrequency(int[] a, int k) {
+        int len=1,l=0,r=0,n=a.length;
+        long sum=0;
+        Arrays.sort(a);
+        while(r<n){
+            sum+=a[r];
+            while((long)(a[r]*(r-l+1))>sum+k){
+                sum=sum-a[l];
+                l++;
+
+            }
+            len=Math.max(r-l+1,len);
+            r++;
+        }
+        return len;
+        
+    }
+}
