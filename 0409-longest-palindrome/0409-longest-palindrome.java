1ff9301e68647b6b8dcbedef527978f80419da02
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-05-25 23:02:07 +0530

    Time: 6 ms (56.78%), Space: 42.5 MB (7.22%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,19 +1,28 @@
 class Solution {
     public int longestPalindrome(String s) {
-       HashSet<Character>map=new HashSet<>();
-       int n=s.length();
-       int res=0;
-       for(char ch:s.toCharArray()){
-        if(!map.contains(ch)){
-            map.add(ch);
+        // Set to store characters that appear an odd number of times
+        HashSet<Character> map = new HashSet<>();
+        int res = 0;
+
+        // Loop through each character in the string
+        for (char ch : s.toCharArray()) {
+            // If this is the first time we see the character, add to set
+            if (!map.contains(ch)) {
+                map.add(ch);
+            } else {
+                // If we see it again (second time), we can form a pair
+                res += 2; // add 2 to the result (pair formed)
+                map.remove(ch); // remove the character from the set
+            }
         }
-        else{
-            res+=2;
-            map.remove(ch);
+
+        // If any characters are left in the set,
+        // that means one of them can be used as the center of the palindrome
+        if (!map.isEmpty()) {
+            res += 1;
         }
-       }
-       if(!map.isEmpty()) return res+1;
-       return res;
-       
+
+        // Return the total length of the longest palindrome
+        return res;
     }
 }
