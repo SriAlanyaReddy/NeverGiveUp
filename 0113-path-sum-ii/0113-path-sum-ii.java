bbada9f659300de41415a353e6b89d6908e7726c
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2025-12-06 23:35:15 +0530

    Time: 1 ms (99.92%), Space: 44.8 MB (71.28%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -15,28 +15,28 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer> ans=new ArrayList<>();
-        findpath(root,targetSum,res,ans);
+        List<List<Integer>>res=new ArrayList<>();
+        List<Integer>l1=new ArrayList<>();
+        pathsumm(root,targetSum,res,l1);
         return res;
-        
     }
-    public void findpath(TreeNode root, int target,List<List<Integer>>ans,List<Integer>temp){
+    public void pathsumm(TreeNode root,int targetSum,List<List<Integer>>res,List<Integer>l1){
         if(root==null){
             return;
         }
-         temp.add(root.val);
-        if(root.val==target && root.left==null && root.right==null){
-            ans.add(new ArrayList<>(temp));
-            temp.remove(temp.size() - 1);
-            return;
+         l1.add(root.val);
+        if(root.left==null && root.right ==null && targetSum==root.val){
+            
+            res.add(new ArrayList<>(l1));
         }
-       else{
-        findpath(root.left,target-root.val,ans,temp);
-        findpath(root.right,target-root.val,ans,temp);
+       
+       
+        //including
+        pathsumm(root.left,targetSum-root.val,res,l1);
+        pathsumm(root.right,targetSum-root.val,res,l1);
+        l1.remove(l1.size()-1);
 
-       }
-        
-        temp.remove(temp.size()-1);
+
     }
+
 }
