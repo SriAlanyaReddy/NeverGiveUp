396d862c03d9c5734bb4144182382ae333da988b
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2026-01-28 23:24:22 +0530

    Time: 111 ms (20.86%), Space: 72 MB (6.44%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -2,21 +2,20 @@ class Solution {
     public int countPrimes(int n) {
     //Sieve Of Erastnaos
      if (n <= 2) return 0;
-    boolean isPrime[]=new boolean[n];
-    Arrays.fill(isPrime,true);
-    isPrime[0]=false;
-    isPrime[1]=false;
+   int isPrime[]=new int[n];
+    Arrays.fill(isPrime,1);
+    
     for(int i=2;i*i<n;i++){
-        if(isPrime[i]==true){
+        if(isPrime[i]==1){
             for(int j=i*i;j<n;j+=i){
-                isPrime[j]=false;
+                isPrime[j]=0;
 
             }
         }
     }
     int c=0;
     for(int i=2;i<n;i++) {
-        if(isPrime[i]==true) c++;
+        if(isPrime[i]==1) c++;
     }
     return c;
     }
