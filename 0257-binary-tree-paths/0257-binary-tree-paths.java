580588dd131c39530772ef5ded02383fc7138ff9
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2026-01-29 03:00:42 +0530

    Time: 1 ms (99.72%), Space: 43.9 MB (92.35%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        dfs(root, new StringBuilder(), res);
+        return res;
+    }
+
+    private void dfs(TreeNode root, StringBuilder sb, List<String> res) {
+        if (root == null) return;
+
+        int len = sb.length();   // save state
+
+        sb.append(root.val);
+
+        if (root.left == null && root.right == null) {
+            // leaf node → add path
+            res.add(sb.toString());
+        } else {
+            sb.append("->");
+            dfs(root.left, sb, res);
+            dfs(root.right, sb, res);
+        }
+
+        sb.setLength(len);  // backtrack correctly
+    }
+}
