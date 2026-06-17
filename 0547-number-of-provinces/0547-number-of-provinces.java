166606eec80e9f6a505795d3e6668017a13f7ce1
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2026-06-17 09:59:23 +0530

    Time: 1 ms (87.07%), Space: 47.4 MB (42.07%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,64 +1,32 @@
-import java.util.HashSet;
-
-class DisjointSet {
-    int parent[];
-    int rank[];
-
-    DisjointSet(int n) {
-        parent = new int[n];
-        rank = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;  // Initially, each node is its own parent
-            rank[i] = 0;    // Initial rank is 0
-        }
-    }
-
-    public int findparent(int x) {
-        if (parent[x] != x) {
-            parent[x] = findparent(parent[x]); // Path compression
-        }
-        return parent[x];
-    }
-
-    public void union(int x, int y) {
-        int rootx = findparent(x);
-        int rooty = findparent(y);
-        
-        if (rootx == rooty) return; // Already connected
-
-        if (rank[rootx] > rank[rooty]) {
-            parent[rooty] = rootx;
-        } else if (rank[rootx] < rank[rooty]) {
-            parent[rootx] = rooty;
-        } else {
-            parent[rooty] = rootx;
-            rank[rootx]++;
-        }
-    }
-}
-
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        DisjointSet ds = new DisjointSet(n);
 
-        // Process connections
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    ds.union(i, j);
-                }
+    public int findCircleNum(int[][] g) {
+
+        int n = g.length;
+        boolean[] vis = new boolean[n];
+
+        int provinces = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(!vis[i]) {
+                provinces++;
+                dfs(g, vis, i);
             }
         }
 
-        // Count unique province roots
-       int c=0;
-        for (int i = 0; i < n; i++) {
-            if(ds.findparent(i)==i){
-                c++;
+        return provinces;
+    }
+
+    public void dfs(int[][] g, boolean[] vis, int node) {
+
+        vis[node] = true;
+
+        for(int adj = 0; adj < g.length; adj++) {
+
+            if(g[node][adj] == 1 && !vis[adj]) {
+                dfs(g, vis, adj);
             }
         }
-
-        return c;
     }
 }
