9437521395190e2525d8db7aa4b59e4eab95093a
SriAlanyaReddy <164356102+SriAlanyaReddy@users.noreply.github.com>
2026-06-17 18:10:21 +0530

    Time: 0 ms (100%), Space: 43.8 MB (11.2%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,35 +1,26 @@
-import java.util.Arrays;
-
 class Solution {
-    public int uniquePathsWithObstacles(int[][] a) {
-        int n = a.length;
-        int m = a[0].length;
+    public int uniquePathsWithObstacles(int[][] mat) {
+     int m=mat.length;
+     int n=mat[0].length;
+     int dp[][]=new int[m][n];
+     for(int[] a:dp){
+        Arrays.fill(a,-1);
+     }
+     return find(m-1,n-1,dp,mat);
 
-        int[][] dp = new int[n][m];
-
-        // Start point
-        if (a[0][0] == 1) return 0;
-        dp[0][0] = 1;
-
-        // Fill first column
-        for (int i = 1; i < n; i++) {
-            if (a[i][0] == 0) dp[i][0] = dp[i-1][0];
+    }
+    public int find(int i,int j,int[][] dp,int[][] a){
+        if(i<0 || j<0 || a[i][j]==1) {
+            return 0;
         }
-
-        // Fill first row
-        for (int j = 1; j < m; j++) {
-            if (a[0][j] == 0) dp[0][j] =dp[0][j-1];
+        if(i==0 && j==0){
+            return 1;
         }
-
-        // Fill the rest of the dp table
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (a[i][j] == 0) {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
+        int val=find(i-1,j,dp,a)+find(i,j-1,dp,a);
+        return dp[i][j]=val;
 
-        return dp[n-1][m-1];
     }
 }
